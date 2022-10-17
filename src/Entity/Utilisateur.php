4e514a82c7075e5a6b987b71909372500fdba761
claudiekante <claudiekante@gmail.com>
2022-10-17 16:44:14 +0200

    creation systeme d'enregistrement

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -76,6 +76,11 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $avatar;
 
+    /**
+     * @ORM\Column(type="string", length=30, unique=true)
+     */
+    private $pseudo;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -233,6 +238,18 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
     public function setAvatar(?string $avatar): self
     {
         $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): self
+    {
+        $this->pseudo = $pseudo;
 
         return $this;
     }
