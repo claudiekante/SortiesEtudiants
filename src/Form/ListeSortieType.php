27c7e9162def30261e678b892d1a2a662dae2550
FrancoisFarlan <francois.farlan@gmail.com>
2022-10-27 11:06:02 +0200

    ajout bouton clear formulaire

--- src/Form/ListeSortieType.php
+++ src/Form/ListeSortieType.php
@@ -7,6 +7,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
